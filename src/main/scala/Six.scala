28866b9bdc275f5027df18e8cecfc261d23e377f
Valerio <valerio@bruttocarattere.org>
2021-12-06 11:22:40 +0100

    fine day 6

--- src/main/scala/Six.scala
+++ src/main/scala/Six.scala
@@ -7,7 +7,7 @@ import scala.annotation.tailrec
 object Six extends App {
 
   def generateDict(dict: Map[Int, BigInt]) = {
-    val foo  = dict.map(x => x._1 - 1 -> x._2)
+    val foo = dict.map(x => x._1 - 1 -> x._2)
     val foo1 = foo + (8 -> foo.getOrElse(8, BigInt.int2bigInt(0)).+(foo.getOrElse(-1, BigInt.int2bigInt(0))))
     val foo2 = foo1 + (6 -> foo1.getOrElse(6, BigInt.int2bigInt(0)).+(foo1.getOrElse(-1, BigInt.int2bigInt(0))))
     val foo3 = foo2 - -1
@@ -16,21 +16,18 @@ object Six extends App {
 
   @tailrec
   def iterateDict(dict: Map[Int, BigInt], day: Int): BigInt = {
-    if(day > 0) {
-      val dictNew = generateDict(dict)
-      iterateDict(dictNew, day -1)
-    } else {
-      dict.values.sum
+    day match {
+      case 0 => dict.values.sum
+      case _ => iterateDict(generateDict(dict), day - 1)
     }
   }
 
   val input = ReadFile.getLines(6).head.split(",").map(_.toInt)
+  var dict = (0 to 8).map { x => x -> BigInt.int2bigInt(input.count(y => y == x)) }.toMap
 
-  var dictImm = (0 to 8).map{x => x -> input.count(y => y == x)}.toMap
-  var dictBig = dictImm.map(x => x._1 -> BigInt.int2bigInt(x._2))
+  var result1 = iterateDict(dict, 80)
+  var result2 = iterateDict(dict, 256)
 
-  var result1 = iterateDict(dictBig, 80)
-  var result2 = iterateDict(dictBig, 256)
   println(result1)
   println(result2)
 
