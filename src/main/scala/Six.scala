1d339de9a52d3b5e0fb9c2ad97bd52a6634d4265
Valerio <valerio@bruttocarattere.org>
2021-12-06 10:47:21 +0100

    less ugly day 6

--- src/main/scala/Six.scala
+++ src/main/scala/Six.scala
@@ -1,29 +1,37 @@
 package dev.scartiloffista
 
-import Six.dict
+import utils.ReadFile
+
+import scala.annotation.tailrec
 
 object Six extends App {
 
-  
+  def generateDict(dict: Map[Int, BigInt]) = {
+    val foo  = dict.map(x => x._1 - 1 -> x._2)
+    val foo1 = foo + (8 -> foo.getOrElse(8, BigInt.int2bigInt(0)).+(foo.getOrElse(-1, BigInt.int2bigInt(0))))
+    val foo2 = foo1 + (6 -> foo1.getOrElse(6, BigInt.int2bigInt(0)).+(foo1.getOrElse(-1, BigInt.int2bigInt(0))))
+    val foo3 = foo2 - -1
+    foo3
+  }
 
-  //val input = "3,4,3,1,2".split(",") map (_.toInt)
-  val input = "1,1,3,5,3,1,1,4,1,1,5,2,4,3,1,1,3,1,1,5,5,1,3,2,5,4,1,1,5,1,4,2,1,4,2,1,4,4,1,5,1,4,4,1,1,5,1,5,1,5,1,1,1,5,1,2,5,1,1,3,2,2,2,1,4,1,1,2,4,1,3,1,2,1,3,5,2,3,5,1,1,4,3,3,5,1,5,3,1,2,3,4,1,1,5,4,1,3,4,4,1,2,4,4,1,1,3,5,3,1,2,2,5,1,4,1,3,3,3,3,1,1,2,1,5,3,4,5,1,5,2,5,3,2,1,4,2,1,1,1,4,1,2,1,2,2,4,5,5,5,4,1,4,1,4,2,3,2,3,1,1,2,3,1,1,1,5,2,2,5,3,1,4,1,2,1,1,5,3,1,4,5,1,4,2,1,1,5,1,5,4,1,5,5,2,3,1,3,5,1,1,1,1,3,1,1,4,1,5,2,1,1,3,5,1,1,4,2,1,2,5,2,5,1,1,1,2,3,5,5,1,4,3,2,2,3,2,1,1,4,1,3,5,2,3,1,1,5,1,3,5,1,1,5,5,3,1,3,3,1,2,3,1,5,1,3,2,1,3,1,1,2,3,5,3,5,5,4,3,1,5,1,1,2,3,2,2,1,1,2,1,4,1,2,3,3,3,1,3,5".split(",") map (_.toInt)
+  @tailrec
+  def iterateDict(dict: Map[Int, BigInt], day: Int): BigInt = {
+    if(day > 0) {
+      val dictNew = generateDict(dict)
+      iterateDict(dictNew, day -1)
+    } else {
+      dict.values.sum
+    }
+  }
 
+  val input = ReadFile.getLines(6).head.split(",").map(_.toInt)
 
   var dictImm = (0 to 8).map{x => x -> input.count(y => y == x)}.toMap
   var dictBig = dictImm.map(x => x._1 -> BigInt.int2bigInt(x._2))
 
+  var result1 = iterateDict(dictBig, 80)
+  var result2 = iterateDict(dictBig, 256)
+  println(result1)
+  println(result2)
 
-  var dict = collection.mutable.Map(dictImm.toSeq: _*).map(x => x._1 -> BigInt.int2bigInt(x._2))
-  
-  for(day <- 1 to 256){
-    dict = dict.map(x => (x._1 - 1, x._2))
-    val foo = dict.getOrElse(8, BigInt.int2bigInt(0))
-    dict(8) = dict.getOrElse(8, BigInt.int2bigInt(0)) + dict.getOrElse(-1, BigInt.int2bigInt(0))
-    dict(6) = dict.getOrElse(6, BigInt.int2bigInt(0)) + dict.getOrElse(-1, BigInt.int2bigInt(0))
-    dict -= -1
-    true
-  }
-
-  println(dict.values.sum)
 }
