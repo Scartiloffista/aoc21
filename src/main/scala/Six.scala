354878f07333d0875e129bb878ddb568229b2bd8
Valerio <valerio@bruttocarattere.org>
2021-12-06 11:30:33 +0100

    doesn't need bigint, just long

--- src/main/scala/Six.scala
+++ src/main/scala/Six.scala
@@ -6,16 +6,16 @@ import scala.annotation.tailrec
 
 object Six extends App {
 
-  def generateDict(dict: Map[Int, BigInt]) = {
+  def generateDict(dict: Map[Int, Long]): Map[Int, Long] = {
     val foo = dict.map(x => x._1 - 1 -> x._2)
-    val foo1 = foo + (8 -> foo.getOrElse(8, BigInt.int2bigInt(0)).+(foo.getOrElse(-1, BigInt.int2bigInt(0))))
-    val foo2 = foo1 + (6 -> foo1.getOrElse(6, BigInt.int2bigInt(0)).+(foo1.getOrElse(-1, BigInt.int2bigInt(0))))
+    val foo1 = foo + (8 -> foo.getOrElse(8, 0L).+(foo.getOrElse(-1, 0L)))
+    val foo2 = foo1 + (6 -> foo1.getOrElse(6, 0L).+(foo1.getOrElse(-1, 0L)))
     val foo3 = foo2 - -1
     foo3
   }
 
   @tailrec
-  def iterateDict(dict: Map[Int, BigInt], day: Int): BigInt = {
+  def iterateDict(dict: Map[Int, Long], day: Int): Long = {
     day match {
       case 0 => dict.values.sum
       case _ => iterateDict(generateDict(dict), day - 1)
@@ -23,7 +23,7 @@ object Six extends App {
   }
 
   val input = ReadFile.getLines(6).head.split(",").map(_.toInt)
-  var dict = (0 to 8).map { x => x -> BigInt.int2bigInt(input.count(y => y == x)) }.toMap
+  var dict = (0 to 8).map { x => x -> input.count(y => y == x).toLong }.toMap
 
   var result1 = iterateDict(dict, 80)
   var result2 = iterateDict(dict, 256)
