6d9a81f78796c8a4634f832f581c88d2bf17f1c2
Valerio <valerio@bruttocarattere.org>
2021-12-11 15:56:54 +0100

    minchia

--- src/main/scala/Eleven.scala
+++ src/main/scala/Eleven.scala
@@ -9,12 +9,14 @@ object Eleven extends App {
 
   val input = ReadFile.getLines(11).map(_.split("").map(_.toInt))
 
-  val indices = input.indices.flatMap(y => input.head.indices.map(x => ((x, y), input(y)(x)))).toMap
+  val indices = input.indices.flatMap(y => input.head.indices.map(x => (x, y) -> input(y)(x))).toMap
   val res1 = p1(indices, 0, 0)
   println(res1)
 
   def getNeigh(p: (Int, Int), input: Map[(Int, Int), Int]) = {
-    Seq(-1, 0, 1).flatMap(x => Seq(-1, 0, 1).map(y => (x, y))).filter(x => x != (0, 0)).map(x => (p._1 + x._1, p._2 + x._2))
+    Seq(-1, 0, 1).flatMap(x => Seq(-1, 0, 1).map(y => (x, y))).filter(x => x != (0, 0))
+      .map(x => (p._1 + x._1, p._2 + x._2))
+      .filter(x => input.contains(x))
   }
 
   @tailrec
@@ -23,7 +25,7 @@ object Eleven extends App {
     if (glowing.isEmpty)
       return input
 
-    val neigh = glowing.toSeq.flatMap(x => getNeigh(x, input)).filter(x => input.contains(x))
+    val neigh = glowing.toSeq.flatMap(x => getNeigh(x, input))
     val step = neigh.foldLeft(input)((step, x) => step + (x -> (step(x) + 1)))
     val glowed = toIgnore concat glowing
     val newGlowing = step.filter(_._2 > 9).filter(x => !glowed.contains(x._1)).keys.toSet
